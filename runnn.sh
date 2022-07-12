550fdada5ea39f1d19b2c1d1af7e8ceb53e6cc88
rg321 <guptaraghu321@gmail.com>
2022-07-12 23:09:57 +0530

    adding runnn.sh file

---
+++ runnn.sh
@@ -0,0 +1,16 @@
+mon=$(date | cut -d ' ' -f 3)
+day=$(date | cut -d ' ' -f 2)
+tim=$(date | cut -d ' ' -f 5)
+
+timestamp=$mon${day}_$tim
+# echo $timestamp
+# echo results_$timestamp.txt
+
+
+
+python3 trainnn.py --lr 0.001 --num_iter 5000 --train_data result_train_lettersOnly \
+--valid_data result_test_lettersOnly --select_data / --batch_ratio 1 \
+--Transformation None --FeatureExtraction VGG --SequenceModeling BiLSTM --Prediction CTC \
+--character "abcdefghijklmnopqrstuvwxyz" --valInterval 5 \
+
+# 2>&1 | tee -a results_$timestamp.txt
