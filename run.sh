91d2272ccd586103713fc7b6a388601727e2afb0
rg321 <guptaraghu321@gmail.com>
2022-07-13 23:19:49 +0530

    making lr 0.1 as default in run.sh

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ fi
 
 echo gpu $gpu
 
-CUDA_VISIBLE_DEVICES=$gpu python3 train.py --lr 0.001 --num_iter 5000 --train_data result_train_lettersOnly \
+CUDA_VISIBLE_DEVICES=$gpu python3 train.py --lr 0.1 --num_iter 5000 --train_data result_train_lettersOnly \
 --valid_data result_test_lettersOnly --select_data / --batch_ratio 1 \
 --Transformation None --FeatureExtraction VGG --SequenceModeling BiLSTM --Prediction CTC \
 --character "abcdefghijklmnopqrstuvwxyz" --valInterval 5 \
